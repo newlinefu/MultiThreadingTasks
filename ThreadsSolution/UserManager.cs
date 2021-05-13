5b8b678b9ced2a49554dd0d992cc89c9dd43f4c6
Alexandr Smirnov <newlinefu@gmail.com>
2021-05-13 16:16:52 +0300

    Prepared

---
+++ ThreadsSolution/UserManager.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+
+namespace ThreadsSolution
+{
+    public class UserManager
+    {
+        public UserManager()
+        {
+        }
+
+        public List<User> InitializeAll(int size = 1500)
+        {
+            List<User> users = new List<User>();
+            
+            for (int i = 0; i < size; i++)
+            {
+                users.Add(new User($"User {i}"));
+            }
+
+            return users;
+        }
+        
+        public void DoWork(List<User> users)
+        {
+            foreach (var user in users)
+            {
+                Console.WriteLine(user.Name);
+                Thread.Sleep(10);
+            }
+        }
+    }
+}
