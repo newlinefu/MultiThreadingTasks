dd760981b70a3bc5eaa76a94f249b716732cf001
Alexandr Smirnov <newlinefu@gmail.com>
2021-05-13 20:46:10 +0300

    Task 2

--- ThreadsSolution/Program.cs
+++ ThreadsSolution/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace ThreadsSolution
@@ -10,17 +11,28 @@ namespace ThreadsSolution
     {
         static void Main(string[] args)
         {
-            Task<int> dividing = new Task<int>(() => Divide(10, 0));
-            dividing.Start();
+            Thread t = new Thread(() =>
+            {
+                try
+                {
+                    Divide(10, 0);
+                }
+                catch (DivideByZeroException e)
+                {
+                    Console.WriteLine("Thread error catch | " + e.Message);
+                }
+            });
+            t.Start();
+            t.Join();
+
+            Task<int> task = new Task<int>(() => Divide(10, 0));
+            task.ContinueWith(
+                tsk => Console.WriteLine("Task error catch | " + tsk?.Exception?.Message), 
+                TaskContinuationOptions.OnlyOnFaulted
+            );
+            task.Start();
             
-            try
-            {
-                Console.WriteLine(dividing.Result);
-            }
-            catch (AggregateException ex)
-            {
-                Console.WriteLine($"Aggregate error catch {ex.Message}");
-            }
+            Thread.Sleep(1000);
         }
 
         private static int Divide(int a, int b)
