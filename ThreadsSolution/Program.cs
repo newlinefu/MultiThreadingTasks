4778e366b2bba9bcc4140972fd5e5f5a90571ff0
Alexandr Smirnov <newlinefu@gmail.com>
2021-05-13 19:56:26 +0300

    Task 5

--- ThreadsSolution/Program.cs
+++ ThreadsSolution/Program.cs
@@ -18,20 +18,35 @@ namespace ThreadsSolution
             List<User> secondPart = users.Skip(50).Take(50).ToList();
             List<User> thirdPart = users.Skip(100).Take(50).ToList();
 
-            // ThreadPool.QueueUserWorkItem((part) => um.DoWork((List<User>) part), firstPart);
-            // ThreadPool.QueueUserWorkItem((part) => um.DoWork((List<User>) part), secondPart);
-            // ThreadPool.QueueUserWorkItem((part) => um.DoWork((List<User>) part), thirdPart);
+            int counter = 0;
+            
+            Stopwatch sw = new Stopwatch();
+            sw.Start();
+            
+            
+            Parallel.Invoke(() =>
+            {
+                um.DoWork(firstPart, CancellationToken.None);
+                Interlocked.Increment(ref counter);
+            },() =>
+            {
+                um.DoWork(secondPart, CancellationToken.None);
+                Interlocked.Increment(ref counter);
+            },() =>
+            {
+                um.DoWork(thirdPart, CancellationToken.None);
+                Interlocked.Increment(ref counter);
+            });
 
-            CancellationTokenSource ct = new CancellationTokenSource();
-            ct.Token.Register(() => Console.WriteLine("Operation stopped"));
-            
-            Task.Run(() => um.DoWork(firstPart, ct.Token), ct.Token);
-            Task.Run(() => um.DoWork(secondPart, ct.Token), ct.Token);
-            Task.Run(() => um.DoWork(thirdPart, ct.Token), ct.Token);
-            
-            Thread.Sleep(500);
-            ct.Cancel();
-            Thread.Sleep(5000);
+            while (true)
+            {
+                if (counter == 3)
+                {
+                    Console.WriteLine(sw.Elapsed);
+                    sw.Reset();
+                    break;
+                }
+            }
         }
     }
 }
