5af864341655d1e92d8675128dae813146d574fc
Alexandr Smirnov <newlinefu@gmail.com>
2021-05-13 20:26:50 +0300

    Clear workspace

--- ThreadsSolution/Program.cs
+++ ThreadsSolution/Program.cs
@@ -9,32 +9,7 @@ namespace ThreadsSolution
     {
         static void Main(string[] args)
         {
-            UserManager um = new UserManager();
-            var users = um.InitializeAll(150);
-
-            List<User> firstPart = users.Take(50).ToList();
-            List<User> secondPart = users.Skip(50).Take(50).ToList();
-            List<User> thirdPart = users.Skip(100).Take(50).ToList();
-
-            Stopwatch sw = new Stopwatch();
-            sw.Start();
             
-            um.DoWork(firstPart);
-            Console.WriteLine(sw.Elapsed);
-            
-            sw.Reset();
-            sw.Start();
-            
-            um.DoWork(secondPart);
-            Console.WriteLine(sw.Elapsed);
-            
-            sw.Reset();
-            sw.Start();
-            
-            um.DoWork(thirdPart);
-            Console.WriteLine(sw.Elapsed);
-            
-            sw.Reset();
         }
     }
 }
