648a4ffdc3caeb33956b857e9b5157138ebd545c
Alexandr Smirnov <newlinefu@gmail.com>
2021-05-13 20:35:56 +0300

    Task 1

--- ThreadsSolution/Program.cs
+++ ThreadsSolution/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
+using System.Threading.Tasks;
 
 namespace ThreadsSolution
 {
@@ -9,7 +10,22 @@ namespace ThreadsSolution
     {
         static void Main(string[] args)
         {
+            Task<int> dividing = new Task<int>(() => Divide(10, 0));
+            dividing.Start();
             
+            try
+            {
+                Console.WriteLine(dividing.Result);
+            }
+            catch (AggregateException ex)
+            {
+                Console.WriteLine($"Aggregate error catch {ex.Message}");
+            }
+        }
+
+        private static int Divide(int a, int b)
+        {
+            return a / b;
         }
     }
 }
